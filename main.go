e97347a5edfd253e684a47d8f5eceabae0992fa5
taiki-nd <taiki.nd.real@gmail.com>
2022-04-28 23:30:51 +0900

    Revert "コードの修正"
    This reverts commit 49ffd01e7af417d1a47579b9798226fea5ed42f8.

--- main.go
+++ main.go
@@ -15,11 +15,6 @@ type User struct {
 	Email     string `gorm:"unique; not null"`
 }
 
-type Address struct {
-	gorm.Model
-	Content string
-}
-
 func main() {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
 		config.Config.DbUserDevelop, config.Config.DbPasswordDevelop, config.Config.DbHostDevelop,
@@ -32,8 +27,8 @@ func main() {
 		panic("failed to connect database")
 	}
 
-	db.Migrator().DropTable(&User{}, &Address{})
-	db.Migrator().CreateTable(&User{}, &Address{}) //emailのunique keyがおかしくなるためテーブルを作り直す
+	db.Migrator().DropTable(&User{})
+	db.Migrator().CreateTable(&User{}) //emailのunique keyがおかしくなるためテーブルを作り直す
 
 	//db.AutoMigrate(&User{})
 	/*
